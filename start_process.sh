bddf0f56ea3abf35f6073079161196c055aaed22
Darci Vieira <darciieira.junior@gmail.com>
2022-08-04 22:32:47 -0300

    ajustes

--- start_process.sh
+++ start_process.sh
@@ -4,6 +4,6 @@ pip install pipenv
 
 pipenv install --system
 
-mv template.html /src/teste-pdf/templates/proposta_judicial.html
+mv template.html templates/proposta_judicial.html
 
-python /src/teste-pdf/main.py 
+python main.py 
